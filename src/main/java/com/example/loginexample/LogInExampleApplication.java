e767c02b39efa8bb8c75af257d9bb8cf008e3dc8
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-27 00:13:27 +0900

    Initial commit

---
+++ src/main/java/com/example/loginexample/LogInExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogInExampleApplication.class, args);
+    }
+
+}
