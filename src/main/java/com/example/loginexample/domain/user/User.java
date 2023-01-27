1888f014028148d3d0224a551cefba19b72b223a
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-27 23:23:59 +0900

    구조 수정

--- src/main/java/com/example/loginexample/domain/user/User.java
+++ src/main/java/com/example/loginexample/domain/user/User.java
@@ -1,4 +1,4 @@
-package com.example.loginexample.domain;
+package com.example.loginexample.domain.user;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
--- src/main/java/com/example/loginexample/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.loginexample.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserRepository {
-
-}
